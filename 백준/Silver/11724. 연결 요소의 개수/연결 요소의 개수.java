26f64040c4fca560c9848c36aea26e327237e245
Kyumho Kim <96820952+kyumho@users.noreply.github.com>
2024-04-27 17:16:24 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 508 ms, Memory: 116792 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -14,6 +14,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+
         N = Integer.parseInt(st1.nextToken());
         M = Integer.parseInt(st1.nextToken());
 
@@ -22,32 +23,38 @@ public class Main {
 
         for (int i = 0; i < M; i++) {
             StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-            int x = Integer.parseInt(st2.nextToken());
-            int y = Integer.parseInt(st2.nextToken());
+            int a = Integer.parseInt(st2.nextToken());
+            int b = Integer.parseInt(st2.nextToken());
 
-            graph[x][y] = 1;
-            graph[y][x] = 1;
+            graph[a][b] = 1;
+            graph[b][a] = 1;
         }
 
         for (int i = 1; i < N + 1; i++) {
+
             if (!visited[i]) {
-                dfs(i);
+                bfs(i);
                 result++;
             }
         }
-
         System.out.println(result);
+
     }
 
-    static void dfs(int point) {
-        if (visited[point]) {
-            return;
-        }
+    static void bfs(int point) {
+        Queue<Integer> q = new LinkedList<>();
 
+        q.offer(point);
         visited[point] = true;
-        for (int i = 1; i < N + 1; i++) {
-            if (graph[point][i] == 1) {
-                dfs(i);
+
+        while (!q.isEmpty()) {
+            int v = q.poll();
+
+            for (int i = 1; i < N + 1; i++) {
+                if (!visited[i] && graph[v][i] == 1) {
+                    q.offer(i);
+                    visited[i] = true;
+                }
             }
         }
     }
