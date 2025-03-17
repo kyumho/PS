93ce814a73e218340fbcdf29f158de7af1463cb4
Kyumho Kim <96820952+kyumho@users.noreply.github.com>
2025-03-17 20:36:52 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,46 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> nums = new LinkedList<>();
-        List<Character> chars = new LinkedList<>();
         String exp = br.readLine();
 
-        String tmp = "";
-        for (int i = 0; i < exp.length(); i++) {
-            if (exp.charAt(i) != '-' && exp.charAt(i) != '+') {
-                tmp += exp.charAt(i);
-            } else {
-                nums.add(Integer.parseInt(tmp));
-                chars.add(exp.charAt(i));
-                tmp = "";
+        int result = 0;
+        String[] nums1 = exp.split("-");
+        String[] firstNum = nums1[0].split("\\+");
+
+        for (String num : firstNum) {
+            result += Integer.parseInt(num);
+        }
+
+        int tmp = 0;
+
+        for (int i = 1; i < nums1.length; i++) {
+            String[] numbers = nums1[i].split("\\+");
+            for (String num : numbers) {
+                tmp += Integer.parseInt(num);
             }
+            result -= tmp;
+            tmp = 0;
         }
 
-        if (tmp.length() != 0){
-            nums.add(Integer.parseInt(tmp));
-        }
-
-        int res = nums.get(0);
-        boolean isMinus = false;
-        for (int i = 0; i < chars.size(); i++) {
-            if (chars.get(i) == '-') {
-                isMinus = true;
-                res -= nums.get(i + 1);
-            } else {
-               if (isMinus) {
-                   res -= nums.get(i + 1);
-               } else {
-                   res += nums.get(i + 1);
-               }
-            }
-        }
-
-        System.out.println(res);
-
+        System.out.println(result);
     }
 }
