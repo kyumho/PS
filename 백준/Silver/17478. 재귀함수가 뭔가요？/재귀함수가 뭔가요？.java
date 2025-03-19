7a6e277af767d1f7f9e9dc74a4082743fbdc407f
Kyumho Kim <96820952+kyumho@users.noreply.github.com>
2025-03-19 18:53:47 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 124 ms, Memory: 14792 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -2,49 +2,38 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+    private static final String[] MESSAGES = {
+            "\"재귀함수가 뭔가요?\"",
+            "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.",
+            "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.",
+            "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\""
+    };
+
+    private static final String RECURSIVE_ANSWER = "\"재귀함수는 자기 자신을 호출하는 함수라네\"";
+    private static final String RESPONSE = "라고 답변하였지.";
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-        String[] sentences = new String[4];
-        sentences[0] = "\"재귀함수가 뭔가요?\"";
-        sentences[1] = "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.";
-        sentences[2] = "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.";
-        sentences[3] = "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"";
-
-        solution(0, n, sentences);
+        recursive(0, n);
     }
 
-    public static void solution(int depth, int n, String[] sentences) {
+    public static void recursive(int depth, int n) {
+        String prefix = "_".repeat(depth * 4);
+
+        System.out.println(prefix + MESSAGES[0]);
 
         if (depth == n) {
-            for (int j = 0; j < depth * 4; j++) {
-                System.out.print("_");
+            System.out.println(prefix + RECURSIVE_ANSWER);
+        } else {
+            for (int i = 1; i < MESSAGES.length; i++) {
+                System.out.println(prefix + MESSAGES[i]);
             }
-            System.out.println("\"재귀함수가 뭔가요?\"");
-            for (int j = 0; j < depth * 4; j++) {
-                System.out.print("_");
-            }
-            System.out.println("\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-            for (int j = 0; j < depth * 4; j++) {
-                System.out.print("_");
-            }
-            System.out.println("라고 답변하였지.");
-            return;
+            recursive(depth + 1, n);
         }
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < depth * 4; j++) {
-                System.out.print("_");
-            }
-            System.out.println(sentences[i]);
-        }
-
-        solution(depth + 1, n, sentences);
-        for (int i = 0; i < depth * 4; i++) {
-            System.out.print("_");
-        }
-        System.out.println("라고 답변하였지.");
+        System.out.println(prefix + RESPONSE);
     }
  }
