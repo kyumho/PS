7fc6c00b6095865c35c9176f8efadca54f0eab16
Kyumho Kim <96820952+kyumho@users.noreply.github.com>
2025-11-26 03:05:24 +0900

    [level 2] Title: 미로 탈출, Time: 6.99 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,66 +1,97 @@
 import java.util.*;
+import java.io.*;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Solution solution = new Solution();
+
+        String[] map = {"SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"};
+
+        solution.solution(map);
+    }
+}
 
 class Solution {
-    int sx, sy, mx, my, ex, ey = 0;
+    int answer = 200;
+
     int[] dx = {0, 0, -1, 1};
     int[] dy = {1, -1, 0, 0};
-    
+
     public int solution(String[] maps) {
-        int answer = 100000;
+        // 시작 지점을 먼저 찾음
+        // 레버를 지나 도착지점까지 간 최소 거리를 구함.
+        // 최소 거리를 구하는데 가중치가 동일하면 dfs보다 bfs 활용!
+        // 못가면 -1 return
         int n = maps.length;
         int m = maps[0].length();
-        char[][] board = new char[n][m];
-        boolean[][][] visited = new boolean[n][m][2];
-        Queue<int[]> q = new LinkedList<>();
-        
-        for (int i = 0; i < n; i++) {
-            board[i] = maps[i].toCharArray();
+
+        int[] start = findChar(maps, 'S');
+        int[] lever = findChar(maps, 'L');
+
+        boolean[][] visited = new boolean[maps.length][maps[0].length()];
+        int distL = bfs(visited, maps, start[0], start[1], n, m, "L");
+
+        boolean[][] visited2 = new boolean[maps.length][maps[0].length()];
+        int distE = bfs(visited2, maps, lever[0], lever[1], n, m, "E");
+
+        System.out.println(distL);
+        System.out.println(distE);
+
+        if (distE == -1 || distL == -1) {
+            return -1;
+        } else {
+            return distL + distE;
         }
-        
+    }
+
+    private int[] findChar(String[] map, Character c) {
+        int n = map.length;
+        int m = map[0].length();
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'S') {
-                    sx = i;
-                    sy = j;
-                } else if (board[i][j] == 'L') {
-                    mx = i;
-                    my = j;
-                } else if (board[i][j] == 'E') {
-                    ex = i;
-                    ey = j;
+                if (map[i].charAt(j) == c) {
+                    return new int[]{i, j};
                 }
             }
         }
-        
-        q.add(new int[]{sx, sy, 0, 0});
-        visited[sx][sy][0] = true;
-        
-        while (!q.isEmpty()) {
-            int[] node = q.poll();
-            
-            if (node[0] == ex && node[1] == ey && node[3] == 1) {
-                answer = node[2];
-                break;
+
+        return new int[]{0, 0};
+    }
+
+    private int bfs(boolean[][] visited, String[] map, int x, int y, int n, int m, String opt) {
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.offer(new int[]{x, y, 0});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int[] q = queue.poll();
+
+            if (map[q[0]].charAt(q[1]) == 'L' && opt.equals("L")) {
+                return q[2];
             }
-            
-            for (int d = 0; d < 4; d++) {
-                int nx = node[0] + dx[d];
-                int ny = node[1] + dy[d];
-                
-                if (0 <= nx && 0 <= ny && nx < n && ny < m) {
-                    if (!visited[nx][ny][node[3]]) {
-                        if (board[nx][ny] == 'L') {
-                            visited[nx][ny][1] = true;
-                            q.offer(new int[]{nx, ny, node[2] + 1, 1});
-                        } else if (board[nx][ny] != 'X') {
-                            visited[nx][ny][node[3]] = true;
-                            q.offer(new int[]{nx, ny, node[2] + 1, node[3]});
-                        }
+
+            if (map[q[0]].charAt(q[1]) == 'E' && opt.equals("E")) {
+                return q[2];
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = q[0] + dx[i];
+                int ny = q[1] + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (!visited[nx][ny] && map[nx].charAt(ny) != 'X') {
+                        visited[nx][ny] = true;
+                        queue.offer(new int[]{nx, ny, q[2] + 1});
                     }
                 }
             }
         }
-        
-        return answer == 100000 ? -1 : answer;
+        return -1;
     }
 }
