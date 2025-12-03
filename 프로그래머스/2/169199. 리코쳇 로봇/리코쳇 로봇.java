8fcdcc06a533b52f0baf2e76934c982d9d0ac8a6
Kyumho Kim <96820952+kyumho@users.noreply.github.com>
2025-12-03 22:08:47 +0900

    [level 2] Title: 리코쳇 로봇, Time: 4.75 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -2,76 +2,89 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    class Node {
+
+    class Robot {
         int x;
         int y;
-        int cnt;
-        
-        public Node(int x, int y, int cnt) {
-            this.x = x;
+        int moveCnt;
+
+        Robot(int x, int y, int moveCnt) {
+            this.x = x ;
             this.y = y;
-            this.cnt = cnt;
+            this.moveCnt = moveCnt;
         }
     }
-    
+
     public int solution(String[] board) {
-        int answer = 0;
-        
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
-        
-        boolean findRobot = false;
-        int startX = 0;
-        int startY = 0;
-        
-        Queue<Node> q = new LinkedList<>();
-        
+
+        int[] dx = {-1 , 1, 0, 0};
+        int[] dy = { 0, 0, -1, 1};
+
         int n = board.length;
         int m = board[0].length();
         boolean[][] visited = new boolean[n][m];
-        
+
+        int[] robot = findPlace(board, n, m, 'R');
+        int robotX = robot[0];
+        int robotY = robot[1];
+
+        int[] goal = findPlace(board, n, m, 'G');
+
+        Queue<Robot> q = new LinkedList<>();
+        q.offer(new Robot(robotX, robotY, 0));
+        visited[robotX][robotY] = true;
+
+        while (!q.isEmpty()) {
+            Robot r = q.poll();
+
+            if (r.x == goal[0] && r.y == goal[1]) {
+                System.out.println(r.moveCnt);
+                return r.moveCnt;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nx = r.x + dx[d];
+                int ny = r.y + dy[d];
+
+                int[] place = moveRobot(board, visited, nx, ny, dx[d], dy[d], n, m);
+
+                if (place[0] >= 0 && place[1] >= 0 && place[0] < n && place[1] < m) {
+                    if (!visited[place[0]][place[1]]) {
+                        visited[place[0]][place[1]] = true;
+                        q.offer(new Robot(place[0], place[1], r.moveCnt + 1));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private int[] moveRobot(String[] board, boolean[][] visited, int nx, int ny, int dx, int dy, int n, int m) {
+
+        // 첫 칸부터 이동 불가능한 경우 처리
+        if (nx < 0 || ny < 0 || nx >= n || ny >= m || board[nx].charAt(ny) == 'D') {
+            return new int[]{-1, -1}; // 유효하지 않은 위치 반환
+        }
+
+        while ((nx + dx >= 0 && ny + dy >= 0 && nx + dx < n && ny + dy < m) && board[nx + dx].charAt(ny + dy) != 'D') {
+                nx += dx;
+                ny += dy;
+        }
+
+        return new int[]{nx, ny};
+    }
+
+    private int[] findPlace(String[] board, int n, int m, Character c) {
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i].charAt(j) == 'R') {
-                    startX = i;
-                    startY = j;
-                    findRobot = true;
-                    break;
-                }
-            }
-            if (findRobot) {
-                break;
-            }
-        }
-        
-        q.add(new Node(startX, startY, 0));
-        
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            
-            for (int d = 0; d < 4; d++) {
-                int nx = cur.x + dx[d];
-                int ny = cur.y + dy[d];
-                
-                while (0 <= nx && 0 <= ny && nx < n && ny < m && board[nx].charAt(ny) != 'D') {
-                    nx += dx[d];
-                    ny += dy[d];
-                }
-                
-                nx -= dx[d];
-                ny -= dy[d];
-                
-                if (board[nx].charAt(ny) == 'G') {
-                    return cur.cnt + 1;
-                }
-                
-                if (board[nx].charAt(ny) == '.' && visited[nx][ny] == false){
-                    visited[nx][ny] = true;
-                    q.add(new Node(nx, ny, cur.cnt + 1));
+                if (board[i].charAt(j) == c) {
+                    return new int[]{i, j};
                 }
             }
         }
-        
-        return -1;
+
+        return new int[]{0, 0};
     }
 }
