3b83e646ba5706765731c8583e15c334b443806c
Kyumho Kim <96820952+kyumho@users.noreply.github.com>
2025-12-04 18:17:23 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 9.40 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,22 +1,24 @@
 class Solution {
+    // 끝에서 배달하고 수거하는게 최소 거리를 보장(?)
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        
+
         long answer = 0;
-        int delivery = 0;
-        int pickup = 0;
-        
+        int deliveryLoad = 0;
+        int pickupLoad = 0;
+
         for (int i = n - 1; i >= 0; i--) {
-            delivery += deliveries[i];
-            pickup += pickups[i];
-            
-            while (delivery > 0 || pickup > 0) {
-                delivery -= cap;
-                pickup -= cap;
-                
-                answer += ((i + 1) * 2);
+            deliveryLoad += deliveries[i];
+            pickupLoad += pickups[i];
+
+            while (deliveryLoad > 0 || pickupLoad > 0) { // 배달, 수거 둘 중 하나가 cap을 넘으면 다시 다녀와야함
+                deliveryLoad -= cap;
+                pickupLoad -= cap;
+                answer += (i + 1) * 2;
             }
         }
-        
+
+        System.out.println(answer);
+
         return answer;
     }
 }
