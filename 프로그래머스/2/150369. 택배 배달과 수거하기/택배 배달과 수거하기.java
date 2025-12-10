4d0dd244a5e1f77b1cf013535f42eb9a83db1100
Kyumho Kim <96820952+kyumho@users.noreply.github.com>
2025-12-10 23:14:26 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 114.18 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,24 +1,71 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
-    // 끝에서 배달하고 수거하는게 최소 거리를 보장(?)
+    // 스택을 사용해서 풀어보기
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
 
         long answer = 0;
-        int deliveryLoad = 0;
-        int pickupLoad = 0;
 
-        for (int i = n - 1; i >= 0; i--) {
-            deliveryLoad += deliveries[i];
-            pickupLoad += pickups[i];
+        Stack<Integer> deliveryStack = new Stack<>();
+        Stack<Integer> pickupStack = new Stack<>();
 
-            while (deliveryLoad > 0 || pickupLoad > 0) { // 배달, 수거 둘 중 하나가 cap을 넘으면 다시 다녀와야함
-                deliveryLoad -= cap;
-                pickupLoad -= cap;
-                answer += (i + 1) * 2;
+        for (int i = 0; i < deliveries.length; i++) {
+            deliveryStack.push(deliveries[i]);
+        }
+
+        for (int i = 0; i < pickups.length; i++) {
+            pickupStack.push(pickups[i]);
+        }
+
+        while (!deliveryStack.isEmpty() || !pickupStack.isEmpty()) {
+           answer += work(cap, n, deliveryStack, pickupStack);
+        }
+
+        System.out.println(answer * 2);
+
+        return answer * 2;
+    }
+
+    private int work(int cap, int n, Stack<Integer> deliveries, Stack<Integer> pickups) {
+        int maxDist = 0;
+        int deliverCnt = 0;
+        int pickupCnt = 0;
+        boolean deliverFirst = true;
+        boolean pickupFirst = true;
+
+        // 배달, 수거 각각 cap에 도달할때까지 채우고 최대 거리 계산
+        while (cap > deliverCnt && !deliveries.isEmpty()) {
+            int nowDeliver =  deliveries.pop();
+
+            if (deliverFirst && nowDeliver > 0) {
+                maxDist = Math.max(maxDist, deliveries.size() + 1);
+                deliverFirst = false;
+            }
+
+            deliverCnt += nowDeliver;
+            if (cap < deliverCnt) {
+                int left = deliverCnt - cap;
+                deliveries.push(left);
+                break;
             }
         }
 
-        System.out.println(answer);
+        while (cap > pickupCnt && !pickups.isEmpty()) {
+            int nowPickup = pickups.pop();
 
-        return answer;
+            if (pickupFirst && nowPickup > 0) {
+                maxDist = Math.max(maxDist, pickups.size() + 1);
+                pickupFirst = false;
+            }
+
+            pickupCnt += nowPickup;
+            if (cap < pickupCnt) {
+                int left = pickupCnt - cap;
+                pickups.push(left);
+                break;
+            }
+        }
+        return maxDist;
     }
 }
